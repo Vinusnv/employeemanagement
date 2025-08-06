9f9b9c260fb767465dd03cd0d556eb1d5d1af9c1
Vinay <vsnv9964@gmail.com>
2025-08-06 23:40:57 +0530

    Here we created GlobalException Handler page

---
+++ src/main/java/com/empmanagement/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.empmanagement.Exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmployeeFoundException.class)
+    public ResponseEntity<?>  EmployeeFoundExceptionHandler(EmployeeFoundException ex){
+
+        Map<String ,String > map = new HashMap<>();
+
+        map.put("errorcode","5240");
+        map.put("errormessage", ex.getMessage());
+
+        return new ResponseEntity<Map<String ,String >>(map, HttpStatus.FOUND);
+
+    }
+
+}
